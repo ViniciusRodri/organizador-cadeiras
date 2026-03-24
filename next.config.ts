76073322532594961987c7c022abc356b2661337
Vinicius Alves Rodrigues <vinicius.arodrigues01@gmail.com>
2026-03-24 20:48:43 -0300

    wip

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/organizador-cadeiras',
+  assetPrefix: '/organizador-cadeiras',
   images: {
     unoptimized: true,
   },
